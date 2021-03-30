d17c8ad2de8b43d2b42d4091b8caf692191e8278
nickyiliwang0000 <70050940+nickyiliwang0000@users.noreply.github.com>
2021-03-30 10:31:31 -0400

    wrapping up learning about interfaces

--- http_interface/main.go
+++ http_interface/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
@@ -14,14 +16,31 @@ func main() {
 		os.Exit(1)
 	}
 
-	// make a byte slice for the read function
-	bs := make([]byte, 99999)
-	// read function translates incoming data into bytes and stores it in the bs
-	resp.Body.Read(bs)
-	// printing out bs as string type
-	fmt.Println(string(bs))
+	// // make a byte slice for the read function
+	// bs := make([]byte, 99999)
+	// // read function translates incoming data into bytes and stores it in the bs
+	// resp.Body.Read(bs)
+	// // printing out bs as string type
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
 
 	// The equivalent of doing the above
-	io.Copy(os.Stdout, resp.Body)
+	// Copy fn expects a dst Writer fn, and src Reader fn, which are satisfied by os.Stdout.Writer, and resp.Body.Reader
+	// copyBuffer then is called and using src dst to output values from http call
 
+	// we pass in our os.stdout (logWriter) method in to copy
+	// it works because Copy expects the Write fn and implicitly gives us a new type interface
+	// any fn that has the Write fn satisfies the interface guideline
+	// this is similar to doing io.Copy(os.stdout, resp.Body)
+
+	io.Copy(lw, resp.Body)
+
+}
+
+// this our implimentation of the os.stdout method in go
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes")
+	return len(bs), nil
 }
